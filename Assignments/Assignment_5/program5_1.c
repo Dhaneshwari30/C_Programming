b44e475ded1ecbf38dcfe457828980984a9ff987
Dhaneshwari Ghewari <shwari2004@gmail.com>
2025-11-10 14:39:57 +0530

    Update program5_1.c

--- Assignments/Assignment_5/program5_1.c
+++ Assignments/Assignment_5/program5_1.c
@@ -1,1 +1,28 @@
 
+//1. Check even odd using if else
+
+#include <stdio.h>
+
+void CheckEvenOdd(int num)
+{
+    if ((num % 2) == 0)
+    {
+        printf("%d is Even", num);
+    }
+    else
+    {
+        printf("%d is Odd", num);
+    }
+}
+
+int main()
+{
+    int number = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &number);
+
+    CheckEvenOdd(number);
+
+    return 0;
+}
