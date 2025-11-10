748ce99b09f3c305ce3123a1e46d02557fd03080
Dhaneshwari Ghewari <shwari2004@gmail.com>
2025-11-10 14:36:38 +0530

    Update program3_1.c

--- Assignments/Assignment_3/program3_1.c
+++ Assignments/Assignment_3/program3_1.c
@@ -1,1 +1,39 @@
+/*1.Write a program which accpet one number from user and print that number or even numbers on screen
 
+Input: 5
+output: 2 4 6 8 10
+
+*/
+
+#include <stdio.h>
+
+void PrintEven(int iNo)
+{
+    int iCnt = 0;
+    int iCal = 0;
+
+    if(iNo <= 0)
+    {
+        return;
+    }
+    else
+    {
+        for(iCnt = 1; iCnt <= iNo; iCnt++)
+        {
+            iCal = 2 * iCnt;
+            printf("  %d ",iCal);
+        }
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number: ");
+    scanf("%d",&iValue);
+
+    PrintEven(iValue);
+
+    return 0;
+}
