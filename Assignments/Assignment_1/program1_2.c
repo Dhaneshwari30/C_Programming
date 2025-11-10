e47121c821ec5064ffe549a84ae98dfb2515c89d
Dhaneshwari Ghewari <shwari2004@gmail.com>
2025-11-10 14:24:17 +0530

    Update program1_2.c

---
+++ Assignments/Assignment_1/program1_2.c
@@ -0,0 +1,27 @@
+///////////////////////////////////////////////////////////////////////
+//
+//  Funtion Name:   Display
+//  Description:    Print marvellours 5 times
+//  Input:          
+//  Output:         string
+//  Author:         Dhaneshwari Prakash Ghewari
+//  Date:           4/11/20205
+///////////////////////////////////////////////////////////////////////
+//2.Program to print 5 times "Marvellous" on screen
+#include <stdio.h>
+
+void Display()
+{
+    int i = 0;
+    for(i = 1; i<=5; i++)
+    {
+        printf("Marvellous\n");
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
