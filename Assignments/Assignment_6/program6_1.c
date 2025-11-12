b6289746f38368c7da48824a85c44ad194e12bb5
Dhaneshwari Ghewari <shwari2004@gmail.com>
2025-11-12 10:46:11 +0530

    Update program6_1.c

--- Assignments/Assignment_6/program6_1.c
+++ Assignments/Assignment_6/program6_1.c
@@ -1,1 +1,15 @@
+//1. Write a program which accept name from user and print that name
 
+#include <stdio.h>
+
+int main()
+{
+    char Name[40];
+
+    printf("Please enter full name: ");
+    scanf("%s", &Name);
+
+    printf("Your name is %s ", Name);
+
+    return 0;
+}
