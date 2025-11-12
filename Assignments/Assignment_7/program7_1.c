853b15bc4e0bc56811453ffa3f7cf272c0af5128
Dhaneshwari Ghewari <shwari2004@gmail.com>
2025-11-12 10:47:31 +0530

    Update program7_1.c

--- Assignments/Assignment_7/program7_1.c
+++ Assignments/Assignment_7/program7_1.c
@@ -1,1 +1,31 @@
+//1. Write a program which accpet number from user and print that number of $ and * on screen
+//input: 5 output: $ * $ * $ * $ * $ *
 
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("&\t*\t");
+    }
+}
+//Time Complexcity: log O(N)
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
