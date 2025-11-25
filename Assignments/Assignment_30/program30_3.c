ca3ec7b0debfaffeb732e744ef2cca466817552c
Dhaneshwari Ghewari <shwari2004@gmail.com>
2025-11-25 11:07:41 +0530

    Add files via upload

---
+++ Assignments/Assignment_30/program30_3.c
@@ -0,0 +1,55 @@
+//2. Accpet number of rows and number of columns from user and display below pattern
+/*
+    iRow = 4
+    iCol = 4
+
+    $ * * *
+    * $ * *
+    * * $ *
+    * * * $
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    if(iRow != iCol)
+    {
+        printf("Invalid input\n");
+        printf("Rows and columns should be same");
+        return;
+    }
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i == j)
+            {
+                printf("$ \t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &iValue1);
+
+    printf("Enter the number of columns: ");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
