8f0e662ea481718cf38fa532c61472edae7e045d
Dhaneshwari Ghewari <shwari2004@gmail.com>
2025-11-26 07:45:27 +0530

    Create program32_1.c

---
+++ Assignments/Assignment_32/program32_1.c
@@ -0,0 +1,55 @@
+//1. Accpet number of rows and number od columns from user and display below patter
+/*
+    iRow = 4
+    iCol = 4
+
+    * * * #
+    * * # *
+    * # * *
+    # * * *
+*/
+#include<stdio.h>
+
+void Patter(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    if(iRow != iCol)
+    {
+        printf("Invalid Input");
+        printf("Number of rows and columns should be same");
+
+        return;
+    }
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(j == (iCol - i) + 1)
+            {
+                printf("#\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &iValue1);
+
+    printf("Enter the number of columns: ");
+    scanf("%d", &iValue2);
+
+    Patter(iValue1, iValue2);
+
+    return 0;
+}
