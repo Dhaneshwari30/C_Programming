fbcab50c4c032615ee837182784b0c281af6d401
Dhaneshwari Ghewari <shwari2004@gmail.com>
2026-02-19 13:47:59 +0530

    Add files via upload

---
+++ Assignments/Assignment_44/program44_4.c
@@ -0,0 +1,148 @@
+//Write a program which count how many times a number appears
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+struct node
+{
+    int Data;
+    struct node *Next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE Head, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->Data = no;
+    newn->Next = NULL;
+
+    if(*Head == NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        newn->Next = *Head;
+        *Head = newn;
+    }
+}
+
+BOOL Search(PNODE Head, int No)
+{
+    BOOL bFlag = FALSE;
+
+    while(Head != NULL)
+    {
+        if(Head->Data == No)
+        {
+            bFlag = TRUE;
+            break;
+        }
+
+        Head = Head->Next;
+    }
+
+    return bFlag;
+}
+
+int CountEven(PNODE Head)
+{
+    int iCnt = 0;
+
+    while(Head != NULL)
+    {
+        if(Head->Data % 2 == 0)
+        {
+            iCnt++;
+        }
+
+        Head = Head->Next;
+    }
+
+    return iCnt;
+}
+
+int CountOdd(PNODE Head)
+{
+    int iCnt = 0;
+
+    while(Head != NULL)
+    {
+        if(Head->Data % 2 != 0)
+        {
+            iCnt++;
+        }
+
+        Head = Head->Next;
+    }
+
+    return iCnt;
+}
+
+int Frequency(PNODE Head, int No)
+{
+    int iCnt = 0;
+
+    while(Head != NULL)
+    {
+        if(Head->Data == No)
+        {
+            iCnt++;
+        }
+
+        Head = Head->Next;
+    }
+
+    return iCnt;
+}
+
+int main()
+{
+    PNODE first = NULL;
+    BOOL bRet = 0;
+    int iRet = 0;
+
+    InsertFirst(&first,101);
+    InsertFirst(&first,51);
+    InsertFirst(&first,6);
+    InsertFirst(&first,11);
+    InsertFirst(&first,21);
+    InsertFirst(&first,28);
+
+    bRet = Search(first, 30);
+
+    if(bRet == TRUE)
+    {
+       printf("Number is present\n"); 
+    }
+    else
+    {
+        printf("Number is not present\n");
+    }
+
+    iRet = CountEven(first);
+
+    printf("Total Even elements: %d\n",iRet);
+
+    iRet = CountOdd(first);
+
+    printf("Total Odd elements: %d\n",iRet);
+
+    iRet = Frequency(first, 101);
+
+    printf("%d\n",iRet);
+
+    return 0;
+
+}
