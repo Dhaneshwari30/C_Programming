3718ec3db8de1e303e07d4b818d02d9ad75046ac
Dhaneshwari Ghewari <shwari2004@gmail.com>
2026-02-22 18:55:19 +0530

    Create program47_1.c

---
+++ Assignments/Assignment_47/program47_1.c
@@ -0,0 +1,72 @@
+//Write a program which Display number that are greater than given number
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+struct node
+{
+    int Data;
+    struct node *Next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE Head, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->Data = no;
+    newn->Next = NULL;
+
+    if(*Head == NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        newn->Next = *Head;
+        *Head = newn;
+    }
+}
+
+void DisplayGreater(PNODE Head, int X)
+{
+    while(Head != NULL)
+    {
+        if(Head->Data > X)
+        {
+            printf("%d\t",Head->Data);
+        }
+
+        Head = Head->Next;
+    }
+}
+
+int main()
+{
+    PNODE first = NULL;
+    BOOL bRet = 0;
+    int iRet = 0;
+
+    InsertFirst(&first,101);
+    InsertFirst(&first,51);
+    InsertFirst(&first,6);
+    InsertFirst(&first,101);
+    InsertFirst(&first,11);
+    InsertFirst(&first,21);
+    InsertFirst(&first,28);
+
+    DisplayGreater(first,30);
+
+    return 0;
+
+}
