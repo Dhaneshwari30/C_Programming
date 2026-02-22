a276855d79f9258e585d84a6bd6fb57ef0328aac
Dhaneshwari Ghewari <shwari2004@gmail.com>
2026-02-22 18:57:55 +0530

    Add files via upload

---
+++ Assignments/Assignment_48/program48_5.c
@@ -0,0 +1,166 @@
+//Write program which display elements at odd positions
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+struct node
+{
+    int Data;
+    struct node *Next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE Head, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->Data = no;
+    newn->Next = NULL;
+
+    if(*Head == NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        newn->Next = *Head;
+        *Head = newn;
+    }
+}
+
+void ReplaceEven(PPNODE Head)
+{
+    PNODE temp = *Head;
+
+    while(temp != NULL)
+    {
+        if((temp->Data % 2) == 0)
+        {
+            temp->Data = 0;
+        }
+
+        temp = temp->Next;
+    }
+}
+
+void ReplaceOdd(PPNODE Head)
+{
+    PNODE temp = *Head;
+
+    while(temp != NULL)
+    {
+        if((temp->Data % 2) != 0)
+        {
+            temp->Data = 0;
+        }
+
+        temp = temp->Next;
+    }
+}
+
+void Display(PNODE Head)
+{
+    while(Head != NULL)
+    {
+        printf("%d\t",Head->Data);
+        Head =  Head->Next;
+    }
+}
+
+void MultiplyByTwo(PPNODE Head)
+{
+    PNODE temp = *Head;
+
+    while(temp != NULL)
+    {
+        temp->Data = temp->Data * 2;
+
+        temp = temp->Next;
+    }
+}
+
+void DisplayEvenPosition(PNODE Head)
+{
+    int iCnt = 1;
+
+    while(Head != NULL)
+    {
+        if(iCnt % 2 == 0)
+        {
+            printf("%d\t",Head->Data);
+        }
+        iCnt++;
+
+        Head = Head->Next;
+    }
+}
+
+void DisplayOddPosition(PNODE Head)
+{
+    int iCnt = 1;
+
+    while(Head != NULL)
+    {
+        if(iCnt % 2 != 0)
+        {
+            printf("%d\t",Head->Data);
+        }
+        iCnt++;
+
+        Head = Head->Next;
+    }
+}
+
+int main()
+{
+    PNODE first = NULL;
+    BOOL bRet = 0;
+    int iRet = 0;
+
+    InsertFirst(&first,101);
+    InsertFirst(&first,51);
+    InsertFirst(&first,6);
+    InsertFirst(&first,101);
+    InsertFirst(&first,11);
+    InsertFirst(&first,21);
+    InsertFirst(&first,28);
+
+    //Display(first);
+
+    //ReplaceEven(&first);
+
+    //printf("\n");
+
+    //Display(first);
+
+    //ReplaceOdd(&first);
+
+    //printf("\n");
+
+    //Display(first);
+
+    //MultiplyByTwo(&first);
+
+    //printf("\n");
+
+    //Display(first);
+
+    DisplayEvenPosition(first);
+
+    printf("\n");
+
+    DisplayOddPosition(first);
+
+    return 0;
+
+}
